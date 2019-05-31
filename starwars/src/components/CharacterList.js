31ab1bf5a2d4ecbd8b6726d8f8db6371fadc0ab3
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-05-31 10:00:54 -0700

    polished a few things

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -3,17 +3,21 @@ import Characters from './Characters';
 import shortid from 'shortid'; 
 
 const CharacterList = props => {
+    console.log(props)
     return (
         <div>
             {props.starwarsChars.map(character => 
-                <Characters 
+                <Characters
                 key= {shortid.generate()}
-                name={character.name}
-                birth_year={character.birth_year}
-                height={character.height}
+                name= {character.name}
+                birth_year= {character.birth_year}
+                height= {character.height}
+                films= {character.films.map(film => {
+                    return film
+                })}
                 />)}
         </div>
     )
 }
 
-export default CharacterList; 
+export default CharacterList;   
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Characters = props => {
-   
+   console.log(props)
     return (
         <div>
             <div className='info-wrapper'>
                 <h2 className='name'>{props.name}</h2>
                 <div className='descrption'>
-                    <p>Birth Year: <span>{props.birth_year}</span></p>
-                    <p>Height: <span>{props.height}</span></p>
+                    <p>Birth Year: {props.birth_year}</p>
+                    <p>Height: {props.height}</p>
                 </div>
             </div>
         </div>
